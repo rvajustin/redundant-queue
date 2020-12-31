aa81f6b24e035419de4fac83c0516f2e86a93081
rvajustin <rvajustin82@gmail.com>
2020-12-30 21:44:11 -0500

    feat: package reference updates

--- src/RVA.RedundantQueue/RVA.RedundantQueue.AspNetCore/RedundantQueueExtensions.cs
+++ src/RVA.RedundantQueue/RVA.RedundantQueue.AspNetCore/RedundantQueueExtensions.cs
@@ -3,17 +3,33 @@ using Microsoft.Extensions.DependencyInjection;
 using RVA.RedundantQueue.Abstractions;
 using RVA.RedundantQueue.Implementations;
 
+// ReSharper disable UnusedType.Global
+// ReSharper disable UnusedMember.Global
+// ReSharper disable MemberCanBePrivate.Global
+
 namespace RVA.RedundantQueue.AspNetCore
 {
     public static class RedundantQueueExtensions
     {
         public static IServiceCollection AddRedundantQueues(IServiceCollection services,
+            Action<IRedundantQueueOrchestrator> callback, out IRedundantQueueOrchestrator redundantQueueOrchestrator)
+        {
+            redundantQueueOrchestrator = new RedundantQueueOrchestrator();
+            callback(redundantQueueOrchestrator);
+            services.AddSingleton(redundantQueueOrchestrator);
+            return services;
+        }
+
+        public static IServiceCollection AddRedundantQueues(IServiceCollection services,
             Action<IRedundantQueueOrchestrator> callback)
         {
-            var orchestrator = new RedundantQueueOrchestrator();
-            callback(orchestrator);
-            services.AddSingleton<IRedundantQueueOrchestrator>(orchestrator);
-            return services;
+            return AddRedundantQueues(services, callback, out _);
+        }
+
+        public static IRedundantQueueOrchestrator AddRedundantQueues(IServiceCollection services)
+        {
+            AddRedundantQueues(services, null, out var redundantQueueOrchestrator);
+            return redundantQueueOrchestrator;
         }
     }
 }
